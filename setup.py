5f9d67518e1cad825747cfb04a697dc9fb71a0e4
priya-kinthali <147703874+priya-kinthali@users.noreply.github.com>
2023-11-23 14:35:36 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
  
 setup(
-    name='samplepython01',
+    name='samplepython02',
     version='1.0',    
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
