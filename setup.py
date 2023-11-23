10c77a85f56f1b9a2b484e460eaaa879657080fe
priyagupta108 <147705955+priyagupta108@users.noreply.github.com>
2023-11-23 13:22:25 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,5 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='Setup',
     version='1.0',    
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
     packages=find_packages()
 )
